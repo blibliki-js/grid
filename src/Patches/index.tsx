060f4c12d0ede4f17dbeec56761c1e7216e14a22
Michalis Zamparas <mikezaby@gmail.com>
2023-04-06 15:31:17 +0300

    Load Patches from the new mechanism

--- src/Patches/index.tsx
+++ src/Patches/index.tsx
@@ -7,21 +7,24 @@ import {
   SelectChangeEvent,
 } from "@mui/material";
 import { useState } from "react";
+import { useLiveQuery } from "dexie-react-hooks";
 
+import db from "models/db";
 import Modal, { close as closeModal } from "components/Modal";
 import { useAppDispatch } from "hooks";
-import Patch from "./Patch";
 
 export default function SavePatch() {
   const dispatch = useAppDispatch();
   const [patchId, setPatchId] = useState("");
 
+  const patches = useLiveQuery(() => db.patches.toArray(), []);
+
   const onSelect = (event: SelectChangeEvent) => setPatchId(event.target.value);
   const select = () => {
-    Patch.load(patchId).then(() => {
-      dispatch(closeModal("patch"));
-    });
+    dispatch(closeModal("patch"));
   };
+
+  if (!patches) return null;
 
   return (
     <Modal modalName="patch">
@@ -34,9 +37,9 @@ export default function SavePatch() {
           label="AudioModule"
           onChange={onSelect}
         >
-          {Patch.availablePatches.map((id) => (
+          {patches.map(({ id, name }) => (
             <MenuItem key={id} value={id}>
-              {id}
+              {name}
             </MenuItem>
           ))}
         </Select>
