af81f80566ce04e28ff6727a74d37b74e0d92b10
Michalis Zamparas <mikezaby@gmail.com>
2023-05-15 17:33:49 +0300

    Add demo patch

--- src/staticPatches/index.ts
+++ src/staticPatches/index.ts
@@ -1,7 +1,8 @@
 import p1 from "./oscSynth.json";
 import p2 from "./threeOscSynth.json";
 import p3 from "./effects.json";
+import p4 from "./demo.json";
 
-const staticPatches = [p1, p2, p3];
+const staticPatches = [p1, p2, p3, p4];
 
 export default staticPatches;
