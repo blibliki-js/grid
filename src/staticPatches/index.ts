a77f33ea411f8cf12fbfe4d51db73bea87325d2b
Michalis Zamparas <mikezaby@gmail.com>
2023-05-15 22:02:17 +0300

    Update staticPatches and bump engine

--- src/staticPatches/index.ts
+++ src/staticPatches/index.ts
@@ -2,7 +2,8 @@ import p1 from "./oscSynth.json";
 import p2 from "./threeOscSynth.json";
 import p3 from "./effects.json";
 import p4 from "./demo.json";
+import p5 from "./spectral.json";
 
-const staticPatches = [p1, p2, p3, p4];
+const staticPatches = [p5, p4, p2, p3, p1];
 
 export default staticPatches;
