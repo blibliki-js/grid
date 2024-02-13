0c8ad2642423515a70c1116ca27797d9d14cf795
Michalis Zamparas <mikezaby@gmail.com>
2024-02-13 15:17:38 +0200

    Remove style dep from keyboard and remove package completely

--- src/components/AudioModule/Keyboard/Key.tsx
+++ src/components/AudioModule/Keyboard/Key.tsx
@@ -1,7 +1,5 @@
 import Engine, { Note } from "@blibliki/engine";
-
 import { useMemo } from "react";
-import styled from "@emotion/styled";
 
 interface StyleProps {
   toneWidth: number;
@@ -10,109 +8,19 @@ interface StyleProps {
   semiToneHeight: number;
 }
 
-const WhiteKey = styled.div<StyleProps>`
-  position: relative;
-  background: #fff;
-  border: 1px solid #000;
-  width: ${(props) => props.toneWidth}px;
-  height: ${(props) => props.toneHeight}px;
-  border-radius: 0 0 5px 5px;
-  margin-right: 1px;
-
-  &.active {
-    box-shadow: inset 0 0 20px rgba(0 0 0 / 25%);
-    transform: scaleY(0.99);
-    -webkit-transform-origin-y: 0;
-  }
-`;
-
-const BlackKey = styled.div<StyleProps>`
-  background: #000;
-  z-index: 2;
-  width: ${(props) => props.semiToneWidth}px;
-  height: ${(props) => props.semiToneHeight}px;
-  border-radius: 0 0 5px 5px;
-
-  &.active {
-    box-shadow: inset 0 0 20px rgba(255 255 255 / 60%);
-    transform: scaleY(0.99);
-    -webkit-transform-origin-y: 0;
-  }
-`;
-
-const C = styled(WhiteKey)`
-  grid-column: 1 / span 2;
-  grid-row: 1 / span 2;
-`;
-
-const CSharp = styled(BlackKey)`
-  grid-column: 2 / span 2;
-  grid-row: 1;
-`;
-
-const D = styled(WhiteKey)`
-  grid-column: 3 / span 2;
-  grid-row: 1 / span 2;
-`;
-
-const DSharp = styled(BlackKey)`
-  grid-column: 5 / span 2;
-  grid-row: 1;
-`;
-
-const E = styled(WhiteKey)`
-  grid-column: 6 / span 2;
-  grid-row: 1 / span 2;
-`;
-
-const F = styled(WhiteKey)`
-  grid-column: 8 / span 2;
-  grid-row: 1 / span 2;
-`;
-
-const FSharp = styled(BlackKey)`
-  grid-column: 9 / span 2;
-  grid-row: 1;
-`;
-
-const G = styled(WhiteKey)`
-  grid-column: 10 / span 2;
-  grid-row: 1 / span 2;
-`;
-
-const GSharp = styled(BlackKey)`
-  grid-column: 12 / span 2;
-  grid-row: 1;
-`;
-
-const A = styled(WhiteKey)`
-  grid-column: 13 / span 2;
-  grid-row: 1 / span 2;
-`;
-
-const ASharp = styled(BlackKey)`
-  grid-column: 15 / span 2;
-  grid-row: 1;
-`;
-
-const B = styled(WhiteKey)`
-  grid-column: 16 / span 2;
-  grid-row: 1 / span 2;
-`;
-
-const Keys: { [key: string]: any } = {
-  C: C,
-  "C#": CSharp,
-  D: D,
-  "D#": DSharp,
-  E: E,
-  F: F,
-  "F#": FSharp,
-  G: G,
-  "G#": GSharp,
-  A: A,
-  "A#": ASharp,
-  B: B,
+const Keys: { [key: string]: string } = {
+  C: "c-key",
+  "C#": "c-sharp-key",
+  D: "d-key",
+  "D#": "d-sharp-key",
+  E: "e-key",
+  F: "f-key",
+  "F#": "f-sharp-key",
+  G: "g-key",
+  "G#": "g-sharp-key",
+  A: "a-key",
+  "A#": "a-sharp-key",
+  B: "b-key",
 };
 
 interface KeyProps extends StyleProps {
@@ -123,26 +31,15 @@ interface KeyProps extends StyleProps {
 }
 
 export default function Key(props: KeyProps) {
-  const {
-    id,
-    note,
-    active,
-    triggerable,
-    toneWidth,
-    toneHeight,
-    semiToneWidth,
-    semiToneHeight,
-  } = props;
+  const { id, note, active, triggerable } = props;
 
   const className = useMemo(() => {
-    const names = [];
+    const names = [Keys[note.name]];
 
     if (active) names.push("active");
 
     return names.join(" ");
-  }, [active]);
-
-  const CurrentKey = useMemo(() => Keys[note.name], [note]);
+  }, [active, note.name]);
 
   const trigger = useMemo(
     () =>
@@ -156,15 +53,11 @@ export default function Key(props: KeyProps) {
   );
 
   return (
-    <CurrentKey
+    <div
       onMouseEnter={trigger("noteOn")}
       onMouseLeave={trigger("noteOff")}
       onMouseDown={trigger("noteOn", true)}
       onMouseUp={trigger("noteOff", true)}
-      toneWidth={toneWidth}
-      toneHeight={toneHeight}
-      semiToneWidth={semiToneWidth}
-      semiToneHeight={semiToneHeight}
       className={className}
     />
   );
