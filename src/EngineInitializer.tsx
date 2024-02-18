38c4b6e1ad8dcdfe5fde781fb42b9c7dcb0d5a24
Michalis Zamparas <mikezaby@gmail.com>
2024-02-18 20:53:55 +0200

    Handle properly the mount/unmount of the app

--- src/EngineInitializer.tsx
+++ src/EngineInitializer.tsx
@@ -1,22 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAppDispatch } from "hooks";
 import { initialize, dispose } from "./globalSlice";
-import { open, close } from "./components/Modal/modalSlice";
 
 export default function EngineInitializer() {
   const dispatch = useAppDispatch();
-  const [enabled, setEnabled] = useState(false);
 
   useEffect(() => {
-    if (enabled) return;
-
-    setEnabled(true);
     dispatch(initialize());
 
     return () => {
       dispatch(dispose());
     };
-  }, [dispatch, enabled]);
+  }, [dispatch]);
 
   return null;
 }
