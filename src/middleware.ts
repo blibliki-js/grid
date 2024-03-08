a61f900c40acf5621b9d22835a6e9eeccb945e83
Michalis Zamparas <mikezaby@gmail.com>
2024-03-08 11:19:48 +0200

    Redirect to /patch/new if you try to access /

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
+  afterAuth(_, req) {
+    if (req.nextUrl.pathname === "/") {
+      return NextResponse.redirect(new URL("/patch/new", req.nextUrl));
+    }
+
+    return NextResponse.next();
+  },
   publicRoutes: () => true,
 });
 
